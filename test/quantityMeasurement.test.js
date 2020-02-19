1ad0061352563e5a3d7c82726af100b98ab960de
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-02-19 15:43:16 +0530

    Added: test case to check reference of given feet value

--- test/quantityMeasurement.test.js
+++ test/quantityMeasurement.test.js
@@ -3,8 +3,13 @@ var quantityMeasurement = require('../main/quantityMeasurement/quantityMeasureme
 
 describe('Quantity Measurement Test Cases', () => {
     it('given 0 feet and 0 feet when equal should return true', () => {
-        var feet1 = quantityMeasurement.convert('FEET', 1)
-        var feet2 = quantityMeasurement.convert('FEET', 1)
+        var feet1 = quantityMeasurement.convert('FEET', 0)
+        var feet2 = quantityMeasurement.convert('FEET', 0)
         assert.equal(feet1, feet2)
+    })
+
+    it('given value in feet should return typeOf', () => {
+        var feet1 = quantityMeasurement.convert('FEET', 0)
+        assert.typeOf(feet1, 'number')
     })
 })
