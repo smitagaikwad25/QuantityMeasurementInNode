7ad99a97817d3275ec6c04e146cf626123c79820
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-02-19 17:20:27 +0530

    Added: functionality to check for yard values

--- main/quantityMeasurement/quantityMeasurement.js
+++ main/quantityMeasurement/quantityMeasurement.js
@@ -6,8 +6,10 @@ module.exports = {
         if (value == null) return false;
         if (unitType == obj[0]) {
             return value * measurement.MEASUREMENT.FEET
-        } else {
+        } else if (unitType == obj[1]) {
             return value * measurement.MEASUREMENT.INCH
+        }else{
+            return value * measurement.MEASUREMENT.YARD
         }
     }
 }
--- test/quantityMeasurement.test.js
+++ test/quantityMeasurement.test.js
@@ -2,6 +2,7 @@ var assert = require('chai').assert
 var quantityMeasurement = require('../main/quantityMeasurement/quantityMeasurement');
 
 describe('Quantity Measurement Test Cases', () => {
+
     it('given 0 feet and 0 feet when equal should return true', () => {
         var feet1 = quantityMeasurement.convert('FEET', 0)
         var feet2 = quantityMeasurement.convert('FEET', 0)
@@ -21,16 +22,58 @@ describe('Quantity Measurement Test Cases', () => {
     it('given values in Inch should return true', () => {
         var valuesInInch = quantityMeasurement.convert('INCH', 0)
         var valuesInInch1 = quantityMeasurement.convert('INCH', 0)
-        assert.equal(valuesInInch,valuesInInch1)
+        assert.equal(valuesInInch, valuesInInch1)
     })
 
     it('given values in Inch should return typeOf', () => {
         var valuesInInch = quantityMeasurement.convert('INCH', 0)
-        assert.typeOf(valuesInInch,'number')
+        assert.typeOf(valuesInInch, 'number')
     })
 
     it('given null values in Inch should return false', () => {
         var valuesInInch = quantityMeasurement.convert('INCH', null)
         assert.isFalse(valuesInInch)
+    })
+
+    it('given values in Feet and Yard when equal should return true', () => {
+        var valuesInInFeet = quantityMeasurement.convert('FEET', 3)
+        var valuesInYard = quantityMeasurement.convert('YARD', 1)
+        assert.equal(valuesInInFeet, valuesInYard)
+    })
+
+    it('given one feet and one Yard should return not equal', () => {
+        var valuesInInFeet = quantityMeasurement.convert('FEET', 1)
+        var valuesInYard = quantityMeasurement.convert('YARD', 1)
+        assert.notEqual(valuesInInFeet, valuesInYard)
+    })
+
+    it('given one inch and one Yard should return not equal', () => {
+        var valuesInInch = quantityMeasurement.convert('INCH', 1)
+        var valuesInYard = quantityMeasurement.convert('YARD', 1)
+        assert.notEqual(valuesInInch, valuesInYard)
+    })
+
+    it('given one inch and one Yard should return not equal', () => {
+        var valuesInInch = quantityMeasurement.convert('INCH', 1)
+        var valuesInYard = quantityMeasurement.convert('YARD', 1)
+        assert.notEqual(valuesInInch, valuesInYard)
+    })
+
+    it('given 1 yard and 36 inches should return not equal', () => {
+        var valuesInYard = quantityMeasurement.convert('YARD', 1)
+        var valuesInInch = quantityMeasurement.convert('INCH', 36)
+        assert.equal(valuesInYard, valuesInInch)
+    })
+
+    it('given 36 inch and 1 yard  should return not equal', () => {
+        var valuesInInch = quantityMeasurement.convert('INCH', 36)
+        var valuesInYard = quantityMeasurement.convert('YARD', 1)
+        assert.equal(valuesInInch, valuesInYard)
+    })
+
+    it('given values 1 yard and 3 feet when equal should return true', () => {
+        var valuesInYard = quantityMeasurement.convert('YARD', 1)
+        var valuesInInFeet = quantityMeasurement.convert('FEET', 3)
+        assert.equal(valuesInYard,valuesInInFeet)
     })
 })
