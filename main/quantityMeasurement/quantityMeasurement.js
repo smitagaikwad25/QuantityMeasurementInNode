117c0048c48ddfaf4e305ab5fb1cc4b3e5d2f76a
smitagaikawad25 <gaikawadsmita021@gmail.com>
2020-02-19 19:08:27 +0530

    Refactored: code to handle Dry-Principle

--- main/quantityMeasurement/quantityMeasurement.js
+++ main/quantityMeasurement/quantityMeasurement.js
@@ -1,15 +1,29 @@
 var measurement = require('./measurement')
-var obj = Object.keys(measurement.MEASUREMENT);
+
+var keys = Object.keys(measurement.MEASUREMENT);
+// console.log(measurement.MEASUREMENT[keys[0]])
 
 module.exports = {
     convert(unitType, value) {
         if (value == null) return false;
-        if (unitType == obj[0]) {
-            return value * measurement.MEASUREMENT.FEET
-        } else if (unitType == obj[1]) {
-            return value * measurement.MEASUREMENT.INCH
-        }else{
-            return value * measurement.MEASUREMENT.YARD
+        for (var i = 0; i < keys.length; i++) {
+            // console.log("value--> " + measurement.MEASUREMENT[keys[i]])
+
+            if (keys[i] == unitType) {
+                console.log(value * measurement.MEASUREMENT[keys[i]]);
+                
+                return value * measurement.MEASUREMENT[keys[i]];
+            }
         }
     }
+    // if (unitType == keys[0]) {
+    //     return value * measurement.MEASUREMENT.FEET
+    // } else if (unitType == keys[1]) {
+    //     return value * measurement.MEASUREMENT.INCH
+    // } else {
+    //     return value * measurement.MEASUREMENT.YARD
+    // }
 }
+
+
+
